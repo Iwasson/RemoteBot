0572c807f7cf0d44c0aebd6730f764eb48bd1b89
Iwasson <Iwasson@users.noreply.github.com>
2020-11-06 15:41:06 -0800

    Update main.js
    Messages only work in #Hack

--- main.js
+++ main.js
@@ -1,80 +1,14 @@
-const fs = require('fs');
-const readline = require('readline');
-const { google } = require('googleapis');
-const spreadId = '1ZvDfvDtjjsV3WZ0N95lC49JUGQfh8VobuaM9FMJ_Oq8';
 const bot = require('node-rocketchat-bot');
 const keys = require('./keys.json');
 const axios = require('axios');
+const { sendToRoom } = require('@rocket.chat/sdk/dist/lib/driver');
 const signAuto = 'https://spot.cat.pdx.edu/api/external/timesheet/sign-auto/';
 const signIn = 'https://spot.cat.pdx.edu/api/external/timesheet/sign-in/';
 const signOut = 'https://spot.cat.pdx.edu/api/external/timesheet/sign-out/';
 const state = 'https://spot.cat.pdx.edu/api/external/timesheet/state/';
 
-
-const SCOPES = ['https://www.googleapis.com/auth/spreadsheets'];
-const TOKEN_PATH = 'token.json';
-
-// Load client secrets from a local file.
-fs.readFile('credentials.json', (err, content) => {
-    if (err) return console.log('Error loading client secret file:', err);
-    // Authorize a client with credentials, then call the Google Sheets API.
-    //authorize(JSON.parse(content), listDevices);
-    authorize(JSON.parse(content), main);
-});
-
-/**
- * Create an OAuth2 client with the given credentials, and then execute the
- * given callback function.
- * @param {Object} credentials The authorization client credentials.
- * @param {function} callback The callback to call with the authorized client.
- */
-function authorize(credentials, callback) {
-    const { client_secret, client_id, redirect_uris } = credentials.installed;
-    const oAuth2Client = new google.auth.OAuth2(
-        client_id, client_secret, redirect_uris[0]);
-
-    // Check if we have previously stored a token.
-    fs.readFile(TOKEN_PATH, (err, token) => {
-        if (err) return getNewToken(oAuth2Client, callback);
-        oAuth2Client.setCredentials(JSON.parse(token));
-        callback(oAuth2Client);
-    });
-}
-
-/**
- * Get and store new token after prompting for user authorization, and then
- * execute the given callback with the authorized OAuth2 client.
- * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
- * @param {getEventsCallback} callback The callback for the authorized client.
- */
-function getNewToken(oAuth2Client, callback) {
-    const authUrl = oAuth2Client.generateAuthUrl({
-        access_type: 'offline',
-        scope: SCOPES,
-    });
-    console.log('Authorize this app by visiting this url:', authUrl);
-    const rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout,
-    });
-    rl.question('Enter the code from that page here: ', (code) => {
-        rl.close();
-        oAuth2Client.getToken(code, (err, token) => {
-            if (err) return console.error('Error while trying to retrieve access token', err);
-            oAuth2Client.setCredentials(token);
-            // Store the token to disk for later program executions
-            fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
-                if (err) return console.error(err);
-                console.log('Token stored to', TOKEN_PATH);
-            });
-            callback(oAuth2Client);
-        });
-    });
-}
-
-function main(auth) {
+function main() {
     //console.log(words);
-
     bot({
         host: keys.host,
         username: keys.username,
@@ -88,17 +22,25 @@ function main(auth) {
         onWake: async event => event.log.info(`${event.bot.username} ready`),
         // on message
         onMessage: async event => {
-            if (event.flags.isMentioned) {
+            if (event.flags.isMentioned && event.room.id == 'GENERAL') {
                 const words = event.message.content.split(' ');
                 const operation = words[1] ? words[1].toLowerCase() : ''
                 event.log.info(`operation is "${operation}"`)
-                processCommand(auth, words, event);
+                processCommand(words, event);
+            }
+            else if(event.flags.isMentioned) {
+                notifyBadChannel(event);
             }
         }
     });
 }
 
-async function processCommand(auth, words, event) {
+async function notifyBadChannel(event) {
+    let user = event.message.author.username;
+    sendToRoom("@" + user + " Please only send me messages here!", 'GENERAL');
+}
+
+async function processCommand(words, event) {
     if (words[1] == null) {
         event.respond("Incorrect Input, please try again or use help");
     }
@@ -206,3 +148,5 @@ async function clock(event, words) {
             })
     }
 }
+
+main();
